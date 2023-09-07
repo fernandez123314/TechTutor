6cadbc0bfed0101e2c666b4d4d58e898af46f293
fernandez123314 <gonzalesmango498@gmail.com>
2023-09-07 15:43:58 -0300

    Nuevas Modificaciones

---
+++ Javascript/index.js
@@ -0,0 +1,6 @@
+function redirigir() {
+    window.location.href = "./Pages/page2.html";
+}
+
+// Llama a la función de redirección después de 5 segundos (5000 milisegundos)
+setTimeout(redirigir, 8000);
